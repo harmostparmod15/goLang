ae830f3fb5e05856ec891526a87ee9bd33cb3abe
= <harmostparmod@gmail.com>
2024-03-30 21:22:24 +0530

    02. variables

---
+++ 02vairables/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+
+func main ( )  {
+
+	// STRING VARIABLE
+var username string = "f"
+fmt.Println(username)
+fmt.Printf("variable is of type :  %T \n"  , username)
+
+
+// BOOLEAN VARIABLE
+var isLoggedIn = true;
+fmt.Println(isLoggedIn)
+fmt.Printf("variable is of type : %T \n " , isLoggedIn)
+
+
+// INTEGER VARIABLE
+var value int = 102
+fmt.Println(value)
+fmt.Printf("variable is of type : %T \n " , value)
+
+
+// FLOAT VARIABLES
+var float float32 = 23.44335
+fmt.Println(float)
+fmt.Printf("variable is of type : %T \n " , float)
+
+
+}
+
